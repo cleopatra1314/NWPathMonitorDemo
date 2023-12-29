f394ce70d4674e97fd8dfbebcaba3d54b9531454
cleopatra <kelly912718@gmail.com>
2023-12-29 19:23:56 +0000

    Finish.

--- NWPathMonitorDemo/ViewController.swift
+++ NWPathMonitorDemo/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import Network
 
 class ViewController: UIViewController {
+    
+    let monitor = NWPathMonitor()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        checkInternet()
+        print("網路連線狀態", monitor.currentPath.status)
     }
 
+    func checkInternet() {
+        monitor.pathUpdateHandler = { path in
+            if path.status == .satisfied {
+                print("⭕️ internet connected successfully.")
+                
+                if path.usesInterfaceType(.wifi) {
+                    print("⭕️ Wifi connected successfully.")
+                } else if path.usesInterfaceType(.cellular) {
+                    print("⭕️ Cellular connected.")
+                } else if path.usesInterfaceType(.wiredEthernet) {
+                    print("⭕️ WiredEthernet connected.")
+                } else {
+                    print("⭕️ Other internet connected.")
+                }
+                print("網路連線狀態", self.monitor.currentPath.status)
+                
+            } else {
+                print("❌ no internet connected.")
+            }
+        }
+        monitor.start(queue: DispatchQueue.global())
+    }
 
 }
 
